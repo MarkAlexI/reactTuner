f4ade4c782571360e33bb07ad36df46c446460c5
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-02-05 10:16:48 +0200

    Added dependencies. Create basic structure of project.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: [
+    './src/Index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        use: 'babel-loader',
+        exclude: /node_modules|cdn_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+      '.jsx'
+    ],
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  resolveLoader: {
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  devServer: {
+    port: 9950,
+    static: {
+      directory: './dist'
+    }
+  }
+};
+
+module.exports = config;
