8f503c127bd2535e9bf870c4df835fdd14dd616b
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-02-21 02:00:44 +0200

    Added some tune to 'strings'.

--- src/utils/strings.tsx
+++ src/utils/strings.tsx
@@ -3,64 +3,64 @@ const strings: StringsStore = {
     "Standard": [
       {
         text: "1-st string",
-        span: "e",
+        span: "e4",
         frequency: 329.63
       },
       {
         text: "2-nd string",
-        span: "B",
+        span: "B3",
         frequency: 246.94
       },
       {
         text: "3-rd string",
-        span: "G",
+        span: "G3",
         frequency: 196
       },
       {
         text: "4-th string",
-        span: "D",
+        span: "D3",
         frequency: 146.83
       },
       {
         text: "5-th string",
-        span: "A",
+        span: "A2",
         frequency: 110
       },
       {
         text: "6-th string",
-        span: "E",
+        span: "E2",
         frequency: 82.41
       }
     ],
     "Drop D": [
       {
         text: "1-st string",
-        span: "e",
+        span: "e4",
         frequency: 329.63
       },
       {
         text: "2-nd string",
-        span: "B",
+        span: "B3",
         frequency: 246.94
       },
       {
         text: "3-rd string",
-        span: "G",
+        span: "G3",
         frequency: 196
       },
       {
         text: "4-th string",
-        span: "D",
+        span: "D3",
         frequency: 146.83
       },
       {
         text: "5-th string",
-        span: "A",
+        span: "A2",
         frequency: 110
       },
       {
         text: "6-th string",
-        span: "D",
+        span: "D2",
         frequency: 73.42
       }
     ],
@@ -69,23 +69,45 @@ const strings: StringsStore = {
     "Standard": [
       {
         text: "1-st string",
-        span: "A",
+        span: "A4",
         frequency: 440
       },
       {
         text: "2-nd string",
-        span: "E",
+        span: "E4",
         frequency: 329.63
       },
       {
         text: "3-rd string",
-        span: "C",
+        span: "C4",
         frequency: 261.63
       },
       {
         text: "4-th string",
-        span: "G",
+        span: "G4",
         frequency: 392
+      }
+    ],
+    "Low G": [
+      {
+        text: "1-st string",
+        span: "A4",
+        frequency: 440
+      },
+      {
+        text: "2-nd string",
+        span: "E4",
+        frequency: 329.63
+      },
+      {
+        text: "3-rd string",
+        span: "C4",
+        frequency: 261.63
+      },
+      {
+        text: "4-th string",
+        span: "G3",
+        frequency: 196
       }
     ],
   }
