45c13f86b03ad9b8d5ee97153b6e7b2cd5423a56
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-03-08 17:04:45 +0200

    Added settings controls to Metronome.

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -3,9 +3,10 @@ import '../assets/styles/Metronome.css';
 
 const Metronome: React.FC = () => {
   let timerId;
-  const tempo: number = 60;
+  const [tempo, setTempo] = useState('60');
+  const [beat, setBeat] = useState('4');
   const [isTurnOn, setTurnOn] = useState(false);
-
+  
   const playBeat = (): void => {
     const audioCtx = new AudioContext();
     let beatNumber: number = 0,
@@ -23,13 +24,13 @@ const Metronome: React.FC = () => {
       gainMaster.gain.exponentialRampToValueAtTime(1, time + 0.001);
       gainMaster.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
 
-      beatNumber = (beatNumber + 1 === 4 ?
+      beatNumber = (beatNumber + 1 === +beat ?
         0 :
         beatNumber + 1);
 
       time += (60.0 / tempo);
     }
-    
+
     osc.stop(time + 0.04);
   };
 
@@ -47,24 +48,57 @@ const Metronome: React.FC = () => {
 
   useEffect(() => {
     if (isTurnOn) {
-      timerId = setInterval(playBeat, (60.0 / tempo) * 4000);
+      timerId = setInterval(playBeat, (60.0 / tempo) * 1000 * +beat);
     }
   }, [isTurnOn]);
 
   return (
-    <div>
-      <button
-        className="play-sound"
-        onClick={on}
-      >
-        Start
-      </button>
-      <button
-        className="play-sound"
-        onClick={off}
-      >
-        Stop
-      </button>
+    <div className="container">
+      <div className="controls">
+        <label>
+          BPM:
+          <span>
+            <p
+              onClick={() => setTempo('' + (+tempo - 1))}
+              className="bpm-minus"
+            >-</p>
+            <input
+              type="text"
+              value={tempo}
+              className="bpm-input"
+              onInput={(e) => setTempo(e.target.value)}
+            />
+            <p
+              onClick={() => setTempo('' +(+tempo + 1))}
+              className="bpm-plus"
+            >+</p>
+          </span>
+          </label>
+          <label>
+            Beat:
+            <input
+              type="text"
+              value={beat}
+              className="ts-top"
+              onInput={(e) => setBeat(e.target.value)}
+            />
+          </label>
+        </div>
+ 
+      <div>
+        <button
+          className="play-sound"
+          onClick={on}
+        >
+          Start
+        </button>
+        <button
+          className="play-sound"
+          onClick={off}
+        >
+          Stop
+        </button>
+      </div>
     </div>
   );
 };
