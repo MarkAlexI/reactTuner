ddd1d54756a00b96741e9a5aae1f362c8a7bf63e
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-03-28 14:32:08 +0300

    Fixed styles.

--- src/App.tsx
+++ src/App.tsx
@@ -11,42 +11,45 @@ export const App: React.FC = () => {
   const [waveType, setWaveType] = useState("sawtooth");
   const [tune, setTune] = useState("Standard");
   const [fiddle, setFiddle] = useState("guitar");
-  
+
   return (
-  <div>
-    <h1>Tuner Demo</h1>
-    Type: {waveType}
-    <br />
-    Tune: {tune}
-    <br />
-    Instrument: {fiddle}
-    <Tabs>
-      <div label="Camerton">
-        <Tuningfork
-          strings={strings[fiddle][tune]}
-          type={waveType}
-          tune={tune}
-        />
-        Press the <em>button</em>!
-      </div>
-      <div label="Tuner">
-        Tune your <em>guitar</em>!
-        <Tuner />
-      </div>
-      <div label="Metronome">
-        Play with <em>metronome</em>!
-        <Metronome />
-      </div>
-      <div label="Settings">
-        <Settings
-          type={waveType}
-          onChangeWaveType={(value) => setWaveType(value)}
-          tune={tune}
-          onChangeTune={(value) => setTune(value)}
-          fiddle={fiddle}
-          onChangeFiddle={(value) => setFiddle(value)}
-        />
-      </div>
-    </Tabs>
+  <div className="wrap">
+    <div className="container">
+      <h1>Tuner</h1>
+      Type: {waveType}
+      <br />
+      Tune: {tune}
+      <br />
+      Instrument: {fiddle}
+      <Tabs>
+        <div label="Camerton">
+          <Tuningfork
+            strings={strings[fiddle][tune]}
+            type={waveType}
+            tune={tune}
+          />
+          Press the <em>button</em>!
+        </div>
+        <div label="Tuner">
+          Tune your <em>guitar</em>!
+          <Tuner />
+        </div>
+        <div label="Metronome">
+          Play with <em>metronome</em>!
+          <Metronome />
+        </div>
+        <div label="Settings">
+          <Settings
+            type={waveType}
+            onChangeWaveType={(value) => setWaveType(value)}
+            tune={tune}
+            onChangeTune={(value) => setTune(value)}
+            fiddle={fiddle}
+            onChangeFiddle={(value) => setFiddle(value)}
+          />
+        </div>
+      </Tabs>
+    </div>
   </div>
-)};
+  )
+};
